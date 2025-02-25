bd60a4ef3279c88ad6f884fa0465d5a14897900c
MCiesiolka <mciesiolka@ivytech.edu>
2025-02-25 09:32:20 -0500

    Delete backend - Copy directory

--- backend - Copy/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://mciesiolka:Bat1724@songdb.j4d3d.mongodb.net/?retryWrites=true&w=majority&appName=songDB")
-
-module.exports = mongoose
-
